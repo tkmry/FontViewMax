ecf5983caa1da1c859bb3714f0995c2105c4c8f7
tkmry <tk.intersect@gmail.com>
2024-09-16 21:44:29 +0900

    Add files via upload
    最初のアップロード

---
+++ FontViewMax.ps1
@@ -0,0 +1,67 @@
+﻿$ScriptRoot = if (-not $PSScriptRoot) {
+    Split-Path -Parent (Convert-Path ([Environment]::GetCommandLineArgs()[0])) 
+} 
+else {
+    $PSScriptRoot # Use the automatic variable.
+}
+
+function Get-UserFontNames {
+    $result = @()
+
+    $font_path = Get-ChildItem (Join-Path $env:LOCALAPPDATA Microsoft\Windows\Fonts\)
+    $font_path | ForEach-Object {
+        $fo = New-Object System.Drawing.Text.PrivateFontCollection
+        $fo.AddFontFile($_.FullName)
+        $result += ,@($_.FullName; $fo.Families.Name)
+    }
+    return $result
+}
+
+function Get-SystemFontNames {
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    return (New-Object System.Drawing.Text.InstalledFontCollection).Families.Name
+}
+
+
+Add-Type -AssemblyName System.Drawing         # フォント操作用
+Add-Type -AssemblyName PresentationFramework  # WPF 用
+Add-Type -AssemblyName System.Windows.Forms   # Timer 用
+
+$libWebView2Wpf    = (Join-Path $ScriptRoot "lib\Microsoft.Web.WebView2.Wpf.dll")
+$libWebView2Core   = (Join-Path $ScriptRoot "lib\Microsoft.Web.WebView2.Core.dll")
+$libWebview2Loader = (Join-Path $ScriptRoot "lib\WebView2Loader.dll")
+
+<# WebView2 用アセンブリロード #>
+[void][reflection.assembly]::LoadFile($libWebView2Wpf)
+[void][reflection.assembly]::LoadFile($libWebView2Core)
+
+<# XAML にて Window 構築 #>
+[xml]$xaml  = (Get-Content (Join-Path $ScriptRoot "etc\ui.xaml"))
+$nodeReader = (New-Object System.XML.XmlNodeReader $xaml)
+$window     = [Windows.Markup.XamlReader]::Load($nodeReader)
+
+$webview  = $window.findName("webView")
+
+<# WebView2 の設定 #>
+$webview.CreationProperties = New-Object 'Microsoft.Web.WebView2.Wpf.CoreWebView2CreationProperties'
+$webview.CreationProperties.UserDataFolder = (Join-Path $ScriptRoot "data")
+$webview.Source = "file:///" + (Join-Path $ScriptRoot "webview/FontViewMax.html")
+
+<# Set EventListener #>
+
+<# WebView2 Messaging #>
+$webview.add_WebMessageReceived({
+    param($webview, $message)
+    $json = ($message.WebMessageAsJson | ConvertFrom-Json)
+    if ($json.status -eq "ready") {
+        $webview.CoreWebView2.PostWebMessageAsJSON((@{
+            system_fonts = Get-SystemFontNames
+            user_fonts   = Get-UserFontNames
+        } | ConvertTo-Json))
+    }
+})
+
+<# Window の表示 #>
+[void]$window.ShowDialog()
+$webview.Dispose()
+$window.Close()
